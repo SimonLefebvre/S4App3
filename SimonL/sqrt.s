6ce67fff64f3f132d133f6bb8ada3a7018be4815
Xavier Dufour <dufourxavi@gmail.com>
2021-06-08 15:30:32 -0400

    Cleanup

--- SimonL/sqrt.s
+++ SimonL/sqrt.s
@@ -1,12 +1,6 @@
 # pour que ces etiquettes soient visibles des autres fichiers par le linker
 .global sqrt_babylon
 .global magnitude
-    
-  /* This is important for an assembly programmer. This
-* directive tells the assembler that don't optimize
-* the order of the instructions as well as don't insert
-* 'nop' instructions after jumps and branches.
-*/
   
 .set noreorder  
   
@@ -14,7 +8,8 @@
     
 .text
 
-
+/*************************************************************************/
+  
  /* a0: value to find the square root of
     a1: precision
     t0: MSB comparison value and initial guess
@@ -31,40 +26,45 @@ sqrt_babylon:
     
     /* Initial guess calculation */
 find_msb:
-    sll $t0, $t6, $t5 /* shift left MSB comparison value*/
-    add $t5, $t5, 1 /* p++ */
-    blt $t0, $a0, find_msb /* Loop until MSB greater than value*/
+    sll $t0, $t6, $t5 # shift left MSB comparison value
+    add $t5, $t5, 1 # p++ 
+    blt $t0, $a0, find_msb # Loop until MSB greater than value
     nop
     
     /* MSB position found */ 
-    sub $t5, $t5, 1 /* Remove 1 from p position counter */
-    div $t0, $t5, 2 /* q=p/2 */
+    sub $t5, $t5, 1 # Remove excess count from position counter
+    div $t0, $t5, 2 # q=p/2 
     
-    addi $t6, $t6, 1 /* dummy value=2 */
-    sll $t0, $t6, $t0 /* x0=2^q */
+    addi $t6, $t6, 1 # dummy value=2 
+    sll $t0, $t6, $t0 # x0=2^q 
     
 iterate:
-    /* Iteration until value is found */
-    div $t1, $a0, $t0 /* S/x0 */
+    # Iteration until value is found 
+    div $t1, $a0, $t0 # S/x0 
     
-    add $t2, $t0, $t1 /* x0+(S/x0) */
+    add $t2, $t0, $t1 # x0+(S/x0) 
     
-    div $t3, $t2, 2 /* (x0 + (S/x0))/2 */ 
+    div $t3, $t2, 2 # (x0 + (S/x0))/2 
     
-    /* Calcul de la condition de convergence */
-    sub $t4, $t0, $t3 /* x_n - x_(n-1) */
-    move $t0, $t3 /* Assign new iteration value */
+    # Calcul de la condition de convergence 
+    sub $t4, $t0, $t3 # x_n - x_(n-1) */
+    move $t0, $t3 # Assign new iteration value 
     
-    bge $t4, $a1, iterate /* Loop if x_n - x_(n-1) > E */
+    bge $t4, $a1, iterate # Loop if x_n - x_(n-1) > E 
     nop
     
-    move $v0, $t3
+    move $v0, $t3 # return value
+    
     jr $ra
     nop
     
 .end sqrt_babylon
-            
     
+/************************************************************************/
+            
+ /* a0: 12-bit ax
+    a1: 12-bit ay
+    a2: 12-bit az */
 .ent magnitude
 magnitude:
     addi $sp, $sp, -4 # reserve word on stack
@@ -84,6 +84,7 @@ magnitude:
     
     lw $ra, 0($sp)    # restore return address
     addi $sp, $sp, 4  # release stack memory
+    
     jr $ra
     nop
     
