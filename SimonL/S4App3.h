033f1ea60d4ca1c0f129c54b2dd199fa3fbe7d48
Jérémy Chenard <jeremy_chenard@hotmail.com>
2021-06-10 14:48:12 -0400

    Ajouter paquet de données et chucksum

--- SimonL/S4App3.h
+++ SimonL/S4App3.h
@@ -34,4 +34,7 @@ uint32_t SendToFlash(uint32_t secondes,int16_t Ax,int16_t Ay,int16_t Az,uint16_t
 void ReadFlash(uint32_t* secondes,int16_t* Ax,int16_t* Ay,int16_t* Az,uint16_t* sqrt,uint16_t* ADC, uint32_t Address, uint16_t Lenght);
 void SendUartData(int16_t* Ax, int16_t* Ay, int16_t* Az, uint16_t* sqrt, uint16_t* ADC);
 void printAllData(int16_t* Ax,int16_t* Ay,int16_t* Az,uint16_t* sqrt,uint16_t* ADC);
+
+void SendToServer(uint32_t* secondes, int16_t* AxCal, int16_t* AyCal, int16_t* AzCal, uint16_t* sqrtCal, uint16_t* ADCCal, 
+        int16_t* Ax, int16_t* Ay, int16_t* Az, uint16_t* sqrt, uint16_t* ADC);
 #endif /* S4APP3_H */
--- SimonL/flashData.c
+++ SimonL/flashData.c
@@ -8,6 +8,8 @@
 #include "S4App3.h"
 
 extern unsigned int magnitude(int ax, int ay, int az);
+extern void parallelPortSend(unsigned char value);
+
 
 static uint32_t secondes[16];
 static int16_t Ax[16];
@@ -57,9 +59,10 @@ void testFlash(void)
         captureCalculSigned(sqrt,donneesSqrt);
         captureCalculSigned(ADC,donneesADC);
         SendUartData(donneesAx,donneesAy,donneesAz,donneesSqrt,donneesADC);
-        printAllData(Ax,Ay, Az, sqrt, ADC);
+        printAllData(Ax, Ay, Az, sqrt, ADC);
         
         
+        SendToServer(secondes, donneesAx, donneesAy, donneesAz, donneesSqrt, donneesADC, Ax, Ay, Az, sqrt, ADC);
     }
     
     
@@ -106,8 +109,7 @@ void ReadFlash(uint32_t* secondes,int16_t* Ax,int16_t* Ay,int16_t* Az,uint16_t* 
         Ay[i] = DataRead[6 + (i*14)]<<8 | DataRead[7 + (i*14)]; 
         Az[i] = DataRead[8 + (i*14)]<<8 | DataRead[9 + (i*14)]; 
         sqrt[i] = DataRead[10 + (i*14)]<<8 | DataRead[11 + (i*14)]; 
-        ADC[i] = DataRead[12 + (i*14)]<<8 | DataRead[13 + (i*14)]; 
-        
+        ADC[i] = DataRead[12 + (i*14)]<<8 | DataRead[13 + (i*14)];  
     }
 }
 
@@ -168,8 +170,178 @@ void SendUartData(int16_t* Ax, int16_t* Ay, int16_t* Az, uint16_t* sqrt, uint16_
     
 }
 
-uint32_t SendToServer(uint16_t secondes, int16_t *Ax, int16_t *Ay, int16_t *Az, uint16_t *sqrt, uint16_t *ADC, uint32_t Address)
-{
+void SendToServer(uint32_t* secondes, int16_t* AxCal, int16_t* AyCal, int16_t* AzCal, uint16_t* sqrtCal, uint16_t* ADCCal, 
+        int16_t* Ax, int16_t* Ay, int16_t* Az, uint16_t* sqrt, uint16_t* ADC)
+{           
+    unsigned char checksum = 0;
     
-    return 0;
+    //Envoie de l'entête
+    parallelPortSend(0x05);
+    
+    //Envoie de la longeur du paquet
+    parallelPortSend(0x10); 
+    parallelPortSend(0x02); 
+
+    //Envoie de l'étiquette de temps
+    parallelPortSend((secondes[15]>>28) & 0X0F); 
+    parallelPortSend((secondes[15]>>24) & 0X0F);
+    parallelPortSend((secondes[15]>>20) & 0X0F); 
+    parallelPortSend((secondes[15]>>16) & 0X0F);
+    parallelPortSend((secondes[15]>>12) & 0X0F);
+    parallelPortSend((secondes[15]>>8) & 0X0F); 
+    parallelPortSend((secondes[15]>>4) & 0X0F);
+    parallelPortSend(secondes[15] & 0X0F); 
+    
+    //Envoie des données calculées
+    for(int i=0; i<3; i++)
+    {
+        parallelPortSend(AxCal[i]>>12); 
+        parallelPortSend((AxCal[i]>>8) & 0X0F); 
+        parallelPortSend((AxCal[i]>>4) & 0X0F); 
+        parallelPortSend(AxCal[i] & 0X0F); 
+        
+        parallelPortSend(AyCal[i]>>12); 
+        parallelPortSend((AyCal[i]>>8) & 0X0F); 
+        parallelPortSend((AyCal[i]>>4) & 0X0F); 
+        parallelPortSend(AyCal[i] & 0X0F);
+        
+        parallelPortSend(AzCal[i]>>12); 
+        parallelPortSend((AzCal[i]>>8) & 0X0F); 
+        parallelPortSend((AzCal[i]>>4) & 0X0F); 
+        parallelPortSend(AzCal[i] & 0X0F);
+        
+        parallelPortSend(sqrtCal[i]>>12); 
+        parallelPortSend((sqrtCal[i]>>8) & 0X0F); 
+        parallelPortSend((sqrtCal[i]>>4) & 0X0F); 
+        parallelPortSend(sqrtCal[i] & 0X0F);
+        
+        parallelPortSend(ADCCal[i]>>12); 
+        parallelPortSend((ADCCal[i]>>8) & 0X0F); 
+        parallelPortSend((ADCCal[i]>>4) & 0X0F); 
+        parallelPortSend(ADCCal[i] & 0X0F);
+    }
+    
+    //Envoie des 16 captures
+    for(int i=0; i<16; i++)
+    {
+        parallelPortSend((secondes[i]>>28) & 0X0F); 
+        parallelPortSend((secondes[i]>>24) & 0X0F);
+        parallelPortSend((secondes[i]>>20) & 0X0F); 
+        parallelPortSend((secondes[i]>>16) & 0X0F);
+        parallelPortSend((secondes[i]>>12) & 0X0F);
+        parallelPortSend((secondes[i]>>8) & 0X0F); 
+        parallelPortSend((secondes[i]>>4) & 0X0F);
+        parallelPortSend(secondes[i] & 0X0F); 
+        
+        parallelPortSend(Ax[i]>>12);
+        parallelPortSend((Ax[i]>>8) & 0X0F); 
+        parallelPortSend((Ax[i]>>4) & 0X0F);
+        parallelPortSend(Ax[i] & 0X0F);        
+        
+        parallelPortSend(Ay[i]>>12);
+        parallelPortSend((Ay[i]>>8) & 0X0F); 
+        parallelPortSend((Ay[i]>>4) & 0X0F);
+        parallelPortSend(Ay[i] & 0X0F);   
+        
+        parallelPortSend(Az[i]>>12);
+        parallelPortSend((Az[i]>>8) & 0X0F); 
+        parallelPortSend((Az[i]>>4) & 0X0F);
+        parallelPortSend(Az[i] & 0X0F);   
+        
+        parallelPortSend(sqrt[i]>>12);
+        parallelPortSend((sqrt[i]>>8) & 0X0F); 
+        parallelPortSend((sqrt[i]>>4) & 0X0F);
+        parallelPortSend(sqrt[i] & 0X0F); 
+        
+        parallelPortSend(ADC[i]>>12);
+        parallelPortSend((ADC[i]>>8) & 0X0F); 
+        parallelPortSend((ADC[i]>>4) & 0X0F);
+        parallelPortSend(ADC[i] & 0X0F);   
+    }
+    
+    //Envoie du CHK
+        //Envoie du CHK
+    checksum = 0x05;
+    
+    checksum ^= (0x10); 
+    checksum ^= (0x02); 
+
+    //Envoie de l'étiquette de temps
+    checksum ^=((secondes[15]>>28) & 0X0F); 
+    checksum ^=((secondes[15]>>24) & 0X0F);
+    checksum ^=((secondes[15]>>20) & 0X0F); 
+    checksum ^=((secondes[15]>>16) & 0X0F);
+    checksum ^=((secondes[15]>>12) & 0X0F);
+    checksum ^=((secondes[15]>>8) & 0X0F); 
+    checksum ^=((secondes[15]>>4) & 0X0F);
+    checksum ^=(secondes[15] & 0X0F); 
+    
+    //Envoie des données calculées
+    for(int i=0; i<3; i++)
+    {
+        checksum ^=(AxCal[i]>>12); 
+        checksum ^=((AxCal[i]>>8) & 0X0F); 
+        checksum ^=((AxCal[i]>>4) & 0X0F); 
+        checksum ^=(AxCal[i] & 0X0F); 
+        
+        checksum ^=(AyCal[i]>>12); 
+        checksum ^=((AyCal[i]>>8) & 0X0F); 
+        checksum ^=((AyCal[i]>>4) & 0X0F); 
+        checksum ^=(AyCal[i] & 0X0F);
+        
+        checksum ^=(AzCal[i]>>12); 
+        checksum ^=((AzCal[i]>>8) & 0X0F); 
+        checksum ^=((AzCal[i]>>4) & 0X0F); 
+        checksum ^=(AzCal[i] & 0X0F);
+        
+        checksum ^=(sqrtCal[i]>>12); 
+        checksum ^=((sqrtCal[i]>>8) & 0X0F); 
+        checksum ^=((sqrtCal[i]>>4) & 0X0F); 
+        checksum ^=(sqrtCal[i] & 0X0F);
+        
+        checksum ^=(ADCCal[i]>>12); 
+        checksum ^=((ADCCal[i]>>8) & 0X0F); 
+        checksum ^=((ADCCal[i]>>4) & 0X0F); 
+        checksum ^=(ADCCal[i] & 0X0F);
+    }
+    
+    //Envoie des 16 captures
+    for(int i=0; i<16; i++)
+    {
+        checksum ^=((secondes[i]>>28) & 0X0F); 
+        checksum ^=((secondes[i]>>24) & 0X0F);
+        checksum ^=((secondes[i]>>20) & 0X0F); 
+        checksum ^=((secondes[i]>>16) & 0X0F);
+        checksum ^=((secondes[i]>>12) & 0X0F);
+        checksum ^=((secondes[i]>>8) & 0X0F); 
+        checksum ^=((secondes[i]>>4) & 0X0F);
+        checksum ^=(secondes[i] & 0X0F); 
+        
+        checksum ^=(Ax[i]>>12);
+        checksum ^=((Ax[i]>>8) & 0X0F); 
+        checksum ^=((Ax[i]>>4) & 0X0F);
+        checksum ^=(Ax[i] & 0X0F);        
+        
+        checksum ^=(Ay[i]>>12);
+        checksum ^=((Ay[i]>>8) & 0X0F); 
+        checksum ^=((Ay[i]>>4) & 0X0F);
+        checksum ^=(Ay[i] & 0X0F);   
+        
+        checksum ^=(Az[i]>>12);
+        checksum ^=((Az[i]>>8) & 0X0F); 
+        checksum ^=((Az[i]>>4) & 0X0F);
+        checksum ^=(Az[i] & 0X0F);   
+        
+        checksum ^=(sqrt[i]>>12);
+        checksum ^=((sqrt[i]>>8) & 0X0F); 
+        checksum ^=((sqrt[i]>>4) & 0X0F);
+        checksum ^=(sqrt[i] & 0X0F); 
+        
+        checksum ^=(ADC[i]>>12);
+        checksum ^=((ADC[i]>>8) & 0X0F); 
+        checksum ^=((ADC[i]>>4) & 0X0F);
+        checksum ^=(ADC[i] & 0X0F);   
+    }
+    
+    parallelPortSend(checksum); 
 }
